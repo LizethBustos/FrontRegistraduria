9734ad9cfed52f337304eefc787cf8fc15d4b053
Luis Miguel Mesa Garcia <73410881+LuisMiguelMesaGarcia@users.noreply.github.com>
2022-11-26 21:01:50 -0500

    Add files via upload

---
+++ candidatos/crear/crear.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Candidato } from '../../../modelos/candidato.model';
+import { CandidatosService } from '../../../servicios/candidato.service';
+
+@Component({
+  selector: 'ngx-crear',
+  templateUrl: './crear.component.html',
+  styleUrls: ['./crear.component.scss']
+})
+export class CrearComponent implements OnInit {
+  modoCreacion: boolean = true;
+  id_candidato: string = "";
+  intentoEnvio: boolean = false;
+  elCandidato: Candidato = {
+    cedula: "",
+    nombre: "",
+    apellido: "",
+    numero_resolucion:""
+  }
+  constructor(private miServicioCandidatos: CandidatosService,
+    private rutaActiva: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    if (this.rutaActiva.snapshot.params.id_candidato) {
+      this.modoCreacion = false;
+      this.id_candidato = this.rutaActiva.snapshot.params.id_candidato;
+      this.getCandidato(this.id_candidato)
+    } else {
+      this.modoCreacion = true;
+    }
+  }
+  getCandidato(id: string) {
+    this.miServicioCandidatos.getCandidato(id).
+      subscribe(data => {
+        this.elCandidato = data;
+      });
+  }
+  agregar(): void {
+    if (this.validarDatosCompletos()) {
+      this.intentoEnvio = true;
+      this.miServicioCandidatos.crear(this.elCandidato).
+        subscribe(data => {
+          Swal.fire(
+            'Creado',
+            'El Candidato ha sido creado correctamente',
+            'success'
+          )
+          this.router.navigate(["pages/candidatos/listar"]);
+        });
+    }
+
+  }
+  editar(): void {
+    this.intentoEnvio = true;
+    if (this.validarDatosCompletos()) {
+      this.miServicioCandidatos.editar(this.elCandidato._id, this.elCandidato).
+        subscribe(data => {
+          Swal.fire(
+            'Actualizado',
+            'El Candidato ha sido actualizado correctamente',
+            'success'
+          )
+          this.router.navigate(["pages/candidatos/listar"]);
+        });
+    }
+
+  }
+  validarDatosCompletos():boolean{
+    this.intentoEnvio=true;
+    if(this.elCandidato.cedula=="" || 
+       this.elCandidato.nombre=="" || 
+       this.elCandidato.apellido==""||
+       this.elCandidato.numero_resolucion==""){
+        
+      return false;
+    }else{
+      return true;
+    }
+  }
+}
---
+++ candidatos/listar/listar.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Candidato } from '../../../modelos/candidato.model';
+import { CandidatosService } from '../../../servicios/candidato.service';
+
+@Component({
+  selector: 'ngx-listar',
+  templateUrl: './listar.component.html',
+  styleUrls: ['./listar.component.scss']
+})
+export class ListarComponent implements OnInit {
+  candidatos : Candidato[];
+  nombresColumnas: string[] = ['Cedula','Nombre','apellido','Numero de resolucion','Opciones'];
+  constructor(private miServicioCandidatos: CandidatosService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.listar();
+  }
+  listar():void{
+    this.miServicioCandidatos.listar().
+      subscribe(data => {
+        this.candidatos=data;
+      });
+  }
+  agregar():void{
+    this.router.navigate(["pages/candidatos/crear"])
+  }
+  editar(id:string):void{
+    this.router.navigate(["pages/candidatos/actualizar/"+id])
+  }
+  eliminar(id:string):void{
+    Swal.fire({
+      title: 'Eliminar Candidato',
+      text: "Está seguro que quiere eliminar el candidato?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.miServicioCandidatos.eliminar(id).
+          subscribe(data => {
+            Swal.fire(
+              'Eliminado!',
+              'El candidato ha sido eliminada correctamente',
+              'success'
+            )
+            this.ngOnInit();
+          });
+      }
+    })
+  }
+}
